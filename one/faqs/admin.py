f296c63b6320c7912ee1c77bc878a9899b5ea1ab
rami.boutassghount <ramiboutas@hotmail.com>
2025-05-10 21:01:13 +0200

    faq cat in admin

--- one/faqs/admin.py
+++ one/faqs/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 
 from one.base.utils.admin import TranslatableModelAdmin
 
-from .models import FAQ
+from .models import FAQ, FAQCategory
 
 
 @admin.register(FAQ)
 class FAQAdmin(TranslatableModelAdmin):
     list_display = ("question", "answer")
     search_fields = ("question", "answer")
+
+
+@admin.register(FAQCategory)
+class FAQCategoryAdmin(TranslatableModelAdmin):
+    pass
