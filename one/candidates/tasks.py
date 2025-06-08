371c7eb0d61a2b2d8620ae0b15cc0e605c68c18a
rami.boutassghount <ramiboutas@hotmail.com>
2025-06-08 23:27:19 +0200

    fix typo

--- one/candidates/tasks.py
+++ one/candidates/tasks.py
@@ -7,7 +7,7 @@ from .models import JobApplication, TexCv
 @huey.db_periodic_task(crontab(minute="*"))
 def task_render_cvs(cv_objs=None):
     if cv_objs is None:
-        cv_objs = TexCv.objects.filter(pdf__in=["", None])
+        cv_objs = TexCv.objects.filter(cv_pdf__in=["", None])
 
     for cv_obj in cv_objs:
         cv_obj.render_cv()
