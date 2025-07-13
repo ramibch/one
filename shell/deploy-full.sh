77bb25121339ce5fc64e82cdf5bf2a062b45155b
rami.boutassghount <ramiboutas@hotmail.com>
2025-07-13 10:08:00 +0200

    deploy scripts

--- shell/deploy-full.sh
+++ shell/deploy-full.sh
@@ -1,4 +1,6 @@
 git pull
 uv run manage.py migrate
+uv run manage.py compilemessages
+uv run manage.py collectstatic --noinput
 sudo systemctl restart huey.service
 sudo systemctl restart gunicorn.service
