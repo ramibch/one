a404f78b647d24f888c18b0f1c90fed583849237
rami.boutassghount <ramiboutas@hotmail.com>
2025-07-16 11:47:30 +0200

    add company_locations field to fix uuid bigint dismatch

--- one/companies/admin.py
+++ one/companies/admin.py
@@ -43,7 +43,7 @@ class PersonAdmin(admin.ModelAdmin):
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    autocomplete_fields = ("recruiter", "company")  # "company_locations",
+    autocomplete_fields = ("company_locations", "recruiter", "company")
     search_fields = ("title", "body")
     list_display = ("__str__", "recruiter", "source_url")
     list_filter = ("language",)
